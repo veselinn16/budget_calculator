f319ec5ca9a64a2909930a59c2749617f1cd20c1
veselinn16 <veselinn16@gmail.com>
2018-08-27 14:07:00 +0300

    Get values of inputs and read them :lock_with_ink_pen:

--- app.js
+++ app.js
@@ -5,14 +5,25 @@ const budgetController = (() => {
 
 // UI Controller
 const UIController = (() => {
-
+    
+    
+    return {
+        getInput: () => {
+            return {
+                type: document.querySelector('.add__type').value, // income or expense
+                description: document.querySelector('.add__description').value,
+                value: document.querySelector('.add__value').value
+            }
+        }
+    }
 })();
 
 // App controller
 const controller = ((UICtrl, budgetCtrl) => {
-    document.querySelector('.add__btn').addEventListener('click', () => {
+    const ctrlAddItem = () => {
         // 1. get input data
-
+        let input = UICtrl.getInput();
+        console.log(input);
         // 2. add item to budget controller
 
         // 3. add new item to the UI
@@ -20,11 +31,13 @@ const controller = ((UICtrl, budgetCtrl) => {
         // 4. calculate budget
 
         // 5. display budget in UI
+    }
 
-    })
+    // click event listener
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
     
     // event listener for ENTER
     document.addEventListener('keypress', (event) => {
-        (event.keyCode === 13) && ()
+        (event.keyCode === 13) && (ctrlAddItem());
     });
 })(UIController, budgetController)
