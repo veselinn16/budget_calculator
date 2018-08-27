2a6b6eac01bdd51a234fbb4ace8fb424b5fa292b
veselinn16 <veselinn16@gmail.com>
2018-08-27 13:06:41 +0300

    Add click event listener to add button :wrench:

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
+// Budget Controller
 const budgetController = (() => {
-    let x = 12;
-
-    let add = (a) => {
-        return a + x;
-    }
-
-    return {
-        publicTest: (b) => {
-            return add(b);
-        }
-    }
+    
 })()
 
+// UI Controller
 const UIController = (() => {
 
 })();
 
+// App controller
 const controller = ((UICtrl, budgetCtrl) => {
-    let z = budgetCtrl.publicTest(1);
-    return {
-        public: () => {
-            console.log(z);
-        }
-    }
+    document.querySelector('.add__btn').addEventListener('click', () => {
+        // 1. get input data
+
+        // 2. add item to budget controller
+
+        // 3. add new item to the UI
+
+        // 4. calculate budget
+
+        // 5. display budget in UI
+
+    })
+    
+    
 })(UIController, budgetController)
-controller.public();
